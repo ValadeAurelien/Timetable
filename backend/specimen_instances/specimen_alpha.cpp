ba7b3c5cceb0371881a9b8891380f93e63391ec4
ValadeAurelien <valade.aurelien69580@gmail.com>
2017-09-18 01:08:53 +0200

    coding

--- backend/specimen_instances/specimen_alpha.cpp
+++ backend/specimen_instances/specimen_alpha.cpp
@@ -1,1 +1,40 @@
 #include "specimen_alpha.hpp"
+#include <cmath>
+
+SpecimenAlpha(const SharedData& _SD) : AbstractSpecimen(_SD)
+{
+
+}
+
+void evaluate()
+{
+    
+}
+
+uchar mutate(AbstractSpecimen *specimen, uchar min, uchar max)
+{
+    uchar nb_mut = 0,
+          nb_mut_todo = floor( (max-min)*((float) rand()/RAND_MAX)+min ); 
+    for (int i=0; i
+}
+
+void changeRoom()
+{
+
+}
+
+void changeHour()
+{
+
+}
+
+void changeTeacher()
+{
+
+}
+
+void changePupils()
+{
+
+}
+
--- backend/specimen_instances/specimen_alpha.hpp
+++ backend/specimen_instances/specimen_alpha.hpp
@@ -1,7 +1,7 @@
 #ifndef SPECIMEN_ALPHA_HEADER
 #define SPECIMEN_ALPHA_HEADER
 
-#include "abstract_classes/abstract_spacimen.hpp"
+#include "abstract_classes/abstract_specimen.hpp"
 #include "shared_data/shared_data.hpp"
 #include "shared_data/shared_intances.hpp"
 #include "basic_instances/pitem_instance.hpp"
@@ -9,14 +9,19 @@
 #include "common/common_types.hpp"
 #include <vector>
 
-class SpecimenAlpha : public AbstractSpecimen
-{
+
+
+class SpecimenAlpha : public AbstractSpecimen {
 public:
     SpecimenAlpha(const SharedData& _SD);
     ~SpecimenAlpha() = default;
     void evaluate() = 0;
     uchar mutate(AbstractSpecimen *specimen, uchar min, uchar max) = 0; 
 private:
-    
+    void changeRoom();
+    void changeHour();
+    void changeTeacher();
+    void changePupils();
 };
+
 #endif

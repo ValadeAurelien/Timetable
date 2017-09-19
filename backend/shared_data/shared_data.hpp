059900d00805b946342ea3f07fa636e6cc1371ab
icannos <icannos.connect-radio@live.fr>
2017-09-19 08:16:30 +0200

    makefile + sharePITem

--- backend/shared_data/shared_data.hpp
+++ backend/shared_data/shared_data.hpp
@@ -1,10 +1,13 @@
 #ifndef SHARED_DATA_HEADER
 #define SHARED_DATA_HEADER
 
+#include <vector>
+#include "shared_data/shared_instances.hpp"
+
 typedef vector<Teacher> TeachersType;
 typedef vector<Room> RoomsType;
 typedef vector<Pupils> PupilssType;
-typedef vector<SharedPitem> SharedPItemsType;
+typedef vector<SharedPItem> SharedPItemsType;
 typedef vector<SharedCourse> SharedCoursesType;
 
 class SharedData
